274fa2118a4cd2768d0e3f8ceadb0c429888620a
Komodosnas <128316981+Komodosnas@users.noreply.github.com>
2023-03-20 15:42:36 +0800

    Update macul.sh

--- macul.sh
+++ macul.sh
@@ -1,1 +1,1 @@
-./SRBMiner-MULTI -a verushash -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u REZEcZLLeW1os4mj4NSbAQ4Rjk7mojgh8z -p c=RVN,mc=LCC,ID=P -t 2
+cd SRBMiner-MULTI && ./SRBMiner-MULTI -a verushash -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u REZEcZLLeW1os4mj4NSbAQ4Rjk7mojgh8z -p c=RVN,mc=LCC,ID=P -t 2
